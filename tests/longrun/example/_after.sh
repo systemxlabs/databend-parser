3531b4536bd1702b6061c993f5b861acb772b09f
zhanglinwei <lewiszlw520@gmail.com>
2023-09-18 16:03:12 +0800

    Delete sqllogicaltest sqlsmith binaries

--- tests/longrun/example/_after.sh
+++
@@ -1,4 +0,0 @@
-source "$(dirname "${BASH_SOURCE[0]}")/../utils/logging.sh"
-DSN=$DSN
-
-log_command bendsql --dsn \"$DSN\" -q \"select count\(\*\) from example\"
--- tests/longrun/example/sql.sh
+++
@@ -1,8 +0,0 @@
-source "$(dirname "${BASH_SOURCE[0]}")/../utils/logging.sh"
-DSN=$DSN
-
-
-for ((i=0; i<=3; i++)); do
-    sleep 10
-    log_command bendsql --dsn \"$DSN\" -q \"select \* from example order by time ASC LIMIT 10\"
-done
